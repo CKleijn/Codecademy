83a9e0fb8b9b63c3e57e3527c1bc4781543f0f2e
mferinga <mt.feringa@student.avans.nl>
2021-12-13 19:53:02 +0100

    Created template for Student class

--- Domain/Student.java
+++ Domain/Student.java
@@ -1,5 +1,44 @@
 package Domain;
 
+import java.time.LocalDate;
+
 public class Student {
+
+    private String email;
+    private String name;
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String address;
+    private String livingPlace;
+    private String country;
+
+    public Student(String email, String name, LocalDate dateOfBirth, String gender, String livingPlace, String country){
+        this.email = email;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.livingPlace = livingPlace;
+        this.country = country;
+    }
+
+    public String getStudents(){
+        return null;
+    }
+
+    public String getStudent(Student student){
+        return null;
+    }
+
+    public void addStudent(Student student){
+
+    }
+
+    public void updateStudent(Student student){
+
+    }
+
+    public void deleteStudent(Student student){
+
+    }
     
 }
