a2322f0992b89b58c77636f0e3b4320f72a01521
CKleijn <cl.kleijn@student.avans.nl>
2021-12-16 20:56:47 +0100

    Changed from 2 SQL files to 1 specific each Class

--- database/StudentSQL.java
+++ database/StudentSQL.java
@@ -1,41 +1,70 @@
 package database;
 
-import java.sql.Date;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class StudentSQL {
-    public void getStudentSQL(ResultSet rs) {
+    public void getStudentSQL(String query) {
+        String connectionUrl = "jdbc:sqlserver://localhost;databaseName=Codecademy;integratedSecurity=true;";
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
         try {
-            String studentEmail = rs.getString("StudentEmail");
-            String studentName = rs.getString("StudentName");
-            Date studentBirthdate = rs.getDate("StudentBirthdate");
-            String studentGender = rs.getString("StudentGender");
-            String studentAddress = rs.getString("StudentAddress");
-            String studentResidence = rs.getString("StudentResidence");
-            String studentCountry = rs.getString("StudentCountry");
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = DriverManager.getConnection(connectionUrl);
 
-            StringBuilder str = new StringBuilder();
+            String SQL = query;
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(SQL);
 
-            str.append("Email: " + studentEmail);
-            str.append("\n");
-            str.append("Name: " + studentName);
-            str.append("\n");
-            str.append("Birthdate: " + studentBirthdate);
-            str.append("\n");
-            str.append("Gender: " + studentGender);
-            str.append("\n");
-            str.append("Address: " + studentAddress);
-            str.append("\n");
-            str.append("Residence: " + studentResidence);
-            str.append("\n");
-            str.append("Country: " + studentCountry);
-            str.append("\n");
+            while (rs.next()) {
+                String studentEmail = rs.getString("StudentEmail");
+                String studentName = rs.getString("StudentName");
+                Date studentBirthdate = rs.getDate("StudentBirthdate");
+                String studentGender = rs.getString("StudentGender");
+                String studentAddress = rs.getString("StudentAddress");
+                String studentResidence = rs.getString("StudentResidence");
+                String studentCountry = rs.getString("StudentCountry");
 
-            System.out.println(str.toString());
-        } 
-        catch (SQLException e) {
+                StringBuilder str = new StringBuilder();
+
+                str.append("Email: " + studentEmail);
+                str.append("\n");
+                str.append("Name: " + studentName);
+                str.append("\n");
+                str.append("Birthdate: " + studentBirthdate);
+                str.append("\n");
+                str.append("Gender: " + studentGender);
+                str.append("\n");
+                str.append("Address: " + studentAddress);
+                str.append("\n");
+                str.append("Residence: " + studentResidence);
+                str.append("\n");
+                str.append("Country: " + studentCountry);
+                str.append("\n");
+
+                System.out.println(str.toString());
+            }
+        }
+
+        catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (rs != null)
+                try {
+                    rs.close();
+                } catch (Exception e) {
+                }
+            if (stmt != null)
+                try {
+                    stmt.close();
+                } catch (Exception e) {
+                }
+            if (con != null)
+                try {
+                    con.close();
+                } catch (Exception e) {
+                }
         }
     }
 }
