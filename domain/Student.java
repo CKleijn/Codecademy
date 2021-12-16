ffeadadedb5ce68ee11a3f90bf69c87221f57d65
CKleijn <cl.kleijn@student.avans.nl>
2021-12-16 20:58:59 +0100

    Changed SQL objects

--- domain/Student.java
+++ domain/Student.java
@@ -1,20 +1,18 @@
 package domain;
 
-import java.time.LocalDate;
-
-import database.SQL;
+import database.StudentSQL;
 
 public class Student {
 
     private String email;
     private String name;
-    private LocalDate dateOfBirth;
+    private String dateOfBirth;
     private String gender;
     private String address;
     private String livingPlace;
     private String country;
 
-    public Student(String email, String name, LocalDate dateOfBirth, String gender, String address, String livingPlace, String country){
+    public Student(String email, String name, String dateOfBirth, String gender, String address, String livingPlace, String country){
         this.email = email;
         this.name = name;
         this.dateOfBirth = dateOfBirth;
@@ -24,33 +22,29 @@ public class Student {
         this.country = country;
     }
 
-    public Student(){
-        
-    }
-
     public void getStudents(){
-        SQL sql = new SQL();
-        sql.connectDB("SELECT * FROM Student");
+        StudentSQL sql = new StudentSQL();
+        sql.getStudentSQL("SELECT * FROM Student");
     }
 
     public void getStudent(Student student){
-        SQL sql = new SQL();
-        sql.connectDB("SELECT * FROM Student WHERE StudentEmail = '" + student.email + "'");
+        StudentSQL sql = new StudentSQL();
+        sql.getStudentSQL("SELECT * FROM Student WHERE StudentEmail = '" + student.email + "'");
     }
 
     public void addStudent(Student student){
-        SQL sql = new SQL();
-        sql.connectDB("INSERT INTO Student VALUES (" + "'" + student.email + "'" + ", " + "'" + student.name + "'" + ", " + "'" + student.dateOfBirth + "'" + ", " + "'" + student.gender + "'" + ", " + "'" + student.address + "'" + ", " + "'" + student.livingPlace  + "'" + ", " + "'" + student.country + "'" + ")");
+        StudentSQL sql = new StudentSQL();
+        sql.getStudentSQL("INSERT INTO Student VALUES (" + "'" + student.email + "'" + ", " + "'" + student.name + "'" + ", " + "'" + student.dateOfBirth + "'" + ", " + "'" + student.gender + "'" + ", " + "'" + student.address + "'" + ", " + "'" + student.livingPlace  + "'" + ", " + "'" + student.country + "'" + ")");
     }
 
     public void updateStudent(Student student){
-        SQL sql = new SQL();
-        sql.connectDB("UPDATE Student SET StudentEmail = '" + student.email + "'" + ", " + "StudentName = '" + student.name + "'" + ", " + "StudentBirthdate = '" + student.dateOfBirth + "'" + ", " + "StudentGender = '" + student.gender + "'" + ", " + "StudentAddress = '" + student.address + "'" + ", " + "StudentResidence = '" + student.livingPlace  + "'" + ", " + "StudentCountry = '" + student.country + "'" + "WHERE StudentEmail = '" + student.email + "'");
+        StudentSQL sql = new StudentSQL();
+        sql.getStudentSQL("UPDATE Student SET StudentEmail = '" + student.email + "'" + ", " + "StudentName = '" + student.name + "'" + ", " + "StudentBirthdate = '" + student.dateOfBirth + "'" + ", " + "StudentGender = '" + student.gender + "'" + ", " + "StudentAddress = '" + student.address + "'" + ", " + "StudentResidence = '" + student.livingPlace  + "'" + ", " + "StudentCountry = '" + student.country + "'" + "WHERE StudentEmail = '" + student.email + "'");
     }
 
     public void deleteStudent(Student student){
-        SQL sql = new SQL();
-        sql.connectDB("DELETE FROM Student WHERE StudentEmail = '" + student.email + "'");
+        StudentSQL sql = new StudentSQL();
+        sql.getStudentSQL("DELETE FROM Student WHERE StudentEmail = '" + student.email + "'");
     }
     
 }
